bfd3cba5023573787a8c596e06774ac699089dad
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-12-01 10:58:27 +0000

    Day 1, part 2

--- day_01/day01_2.go
+++ day_01/day01_2.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 )
 
-func processFile(filename string, frequency *int) {
+func processFile(filename string, frequency *int, seenMap map[int]bool) (bool, int) {
 	file, err := os.Open(filename)
 	defer file.Close()
 	if err != nil {
@@ -22,20 +22,33 @@ func processFile(filename string, frequency *int) {
 	for scanner.Scan() {
 		operation, _ := strconv.Atoi(scanner.Text())
 		*frequency += operation
+		if _, ok := seenMap[*frequency]; !ok {
+			seenMap[*frequency] = true
+		} else {
+			return true, *frequency
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	return false, 0
 }
 
 func main() {
 	var frequency int
+	var seen int
+	var finished bool = false
+	seenMap := make(map[int]bool)
+	seenMap[0] = true
+
 	args := os.Args[1:]
 	if len(args) != 1 {
 		log.Fatal("You must supply a file to process.")
 	}
 	filename := args[0]
-	processFile(filename, &frequency)
-	fmt.Printf("Final frequency: %d\n", frequency)
+	for finished == false {
+		finished, seen = processFile(filename, &frequency, seenMap)
+	}
+	fmt.Printf("Repeated frequency: %d\n", seen)
 }
