05bc66e372a086d17180c080cfbbef5b023102a4
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-12-13 05:42:47 +0000

    Day 9 Part 1 and Part 2

--- day_09/day09_1.go
+++ day_09/day09_1.go
@@ -16,50 +16,107 @@ type Players struct {
 }
 
 type Marble struct {
-	Value int
+	Value    int
+	Next     *Marble
+	Previous *Marble
 }
 
 type MarbleCircle struct {
-	Marbles         []Marble
+	CurrentMarble   *Marble
+	FirstMarble     *Marble
 	Size            int
 	CurrentPosition int
 }
 
-func (circle *MarbleCircle) CalculateNextPosition(increment int) int {
-	var position int = (circle.CurrentPosition + increment)
-	if position < 0 {
-		position = -position
+//func (circle *MarbleCircle) CalculateNextPosition(increment int) int {
+//	var position int = (circle.CurrentPosition + increment)
+//	if position < 0 {
+//		position = -position
+//	}
+//	return position % circle.Size
+//}
+//
+//func (circle *MarbleCircle) AddMarble(value int) {
+//
+//	var position1 int = circle.CalculateNextPosition(1)
+//
+//	circle.Marbles = append(circle.Marbles, Marble{Value: -1})
+//	circle.Size++
+//	var position2 int = circle.CalculateNextPosition(2)
+//
+//	copy(circle.Marbles[position2+1:], circle.Marbles[position2:circle.Size])
+//	circle.Marbles[position1+1].Value = value
+//
+//	circle.CurrentPosition = position1 + 1
+//}
+//
+//func (circle *MarbleCircle) RemoveMarble(offsetToRemove int) int {
+//
+//	var position int = circle.CalculateNextPosition(offsetToRemove)
+//	var value int = circle.Marbles[position].Value
+//
+//	newCircle := make([]Marble, circle.Size-1)
+//
+//	copy(newCircle[:position], circle.Marbles[:position])
+//	copy(newCircle[position:], circle.Marbles[position+1:])
+//	circle.Size--
+//
+//	circle.CurrentPosition = position
+//	circle.Marbles = newCircle
+//	return value
+//}
+
+func (marbles *MarbleCircle) Show() {
+	var current *Marble
+
+	current = marbles.FirstMarble
+	fmt.Printf("[ ")
+	fmt.Printf("%d ", (*current).Value)
+	current = (*current).Next
+	for current != marbles.FirstMarble {
+		fmt.Printf("%d ", (*current).Value)
+		current = (*current).Next
 	}
-	return position % circle.Size
+	fmt.Printf("]\n")
 }
 
-func (circle *MarbleCircle) AddMarble(value int) {
+func (marbles *MarbleCircle) AddMarble(value int) {
+	var newMarble Marble
 
-	var position1 int = circle.CalculateNextPosition(1)
+	newMarble.Value = value
 
-	circle.Marbles = append(circle.Marbles, Marble{Value: -1})
-	circle.Size++
-	var position2 int = circle.CalculateNextPosition(2)
+	// Next Position
+	marbles.CurrentMarble = marbles.CurrentMarble.Next
+	marbles.CurrentMarble.Next.Previous = &newMarble
+	newMarble.Next = marbles.CurrentMarble.Next
+	newMarble.Previous = marbles.CurrentMarble
+	marbles.CurrentMarble.Next = &newMarble
+	marbles.CurrentMarble = marbles.CurrentMarble.Next
 
-	copy(circle.Marbles[position2+1:], circle.Marbles[position2:circle.Size])
-	circle.Marbles[position1+1].Value = value
+	marbles.Size++
 
-	circle.CurrentPosition = position1 + 1
 }
 
-func (circle *MarbleCircle) RemoveMarble(offsetToRemove int) int {
+func (marbles *MarbleCircle) RemoveMarble(offsetToRemove int) int {
 
-	var position int = circle.CalculateNextPosition(offsetToRemove)
-	var value int = circle.Marbles[position].Value
+	var value int
+	var auxMarble *Marble
 
-	newCircle := make([]Marble, circle.Size-1)
+	for i := 0; i < offsetToRemove; i++ {
+		marbles.CurrentMarble = marbles.CurrentMarble.Previous
+	}
+	value = marbles.CurrentMarble.Value
+	auxMarble = marbles.CurrentMarble
+	marbles.CurrentMarble = marbles.CurrentMarble.Next
+	auxMarble.Previous.Next = marbles.CurrentMarble
+	marbles.CurrentMarble.Previous = auxMarble.Previous
 
-	copy(newCircle[:position], circle.Marbles[:position])
-	copy(newCircle[position:], circle.Marbles[position+1:])
-	circle.Size--
+	auxMarble.Next = nil
+	auxMarble.Previous = nil
+	auxMarble.Value = -1
 
-	circle.CurrentPosition = position
-	circle.Marbles = newCircle
+	marbles.Size--
+
 	return value
 }
 
@@ -73,7 +130,7 @@ func play(circle MarbleCircle, players Players, lastValue int) int {
 			circle.AddMarble(newValue)
 		} else {
 			players.Points[players.Current] += newValue
-			players.Points[players.Current] += circle.RemoveMarble(-7)
+			players.Points[players.Current] += circle.RemoveMarble(7)
 		}
 		newValue++
 		players.Current = (players.Current + 1) % players.NumberOfPlayers
@@ -90,7 +147,14 @@ func play(circle MarbleCircle, players Players, lastValue int) int {
 func main() {
 
 	var numberOfPlayers, lastValue int
-	circle := MarbleCircle{Size: 1, CurrentPosition: 0, Marbles: make([]Marble, 1)}
+	var firstMarble Marble
+	marbles := MarbleCircle{Size: 1, CurrentPosition: 0}
+
+	firstMarble.Value = 0
+	firstMarble.Next = &firstMarble
+	firstMarble.Previous = &firstMarble
+	marbles.CurrentMarble = &firstMarble
+	marbles.FirstMarble = &firstMarble
 
 	args := os.Args[1:]
 
@@ -103,6 +167,6 @@ func main() {
 
 	players := Players{Current: 1, NumberOfPlayers: numberOfPlayers, Points: map[int]int{}}
 
-	fmt.Printf("Score :%d\n", play(circle, players, lastValue))
+	fmt.Printf("Score :%d\n", play(marbles, players, lastValue))
 
 }
